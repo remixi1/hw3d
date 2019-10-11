3caf1ee5595ec0b7907193ccba68d6d3e047badd
Jani Reima <remixi1@hotmail.com>
2019-10-12 01:48:04 +0300

    Hexagon

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -110,15 +110,29 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 
 	struct Vertex
 	{
+		struct 
+		{
 		float x;
 		float y;
+		} pos;
+		struct
+		{
+		unsigned char r;
+		unsigned char g;
+		unsigned char b;
+		unsigned char a;
+		} color;
 	};
-	const Vertex vertices[] =
+	Vertex vertices[] =
 	{
-		{ 0.0f,0.5f },
-	{ 0.5f,-0.5f },
-	{ -0.5f,-0.5f },
+	{ 0.0f,0.5f,255,0,0,0},
+	{ 0.5f,-0.5f,0,255,0,0},
+	{ -0.5f,-0.5f,0,0,255,0},
+	{ -0.3f,0.3f,0,255,0,0},
+	{ 0.3f,0.3f,0,0,255,0},
+	{ 0.0f,-0.8f,255,0,0,0},
 	};
+	vertices[0].color.g = 255;
 
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	D3D11_BUFFER_DESC bd = {};
@@ -132,6 +146,37 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 	sd.pSysMem = vertices;
 	GFX_THROW_INFO(pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer));
 
+
+
+	//Bind vertex buffer to pipeline
+	const UINT stride = sizeof(Vertex);
+	const UINT offset = 0u;
+	pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+
+	//Create index buffer
+	const unsigned short indices[] =
+	{
+		0,1,2,
+		0,2,3,
+		0,4,1,
+		2,1,5,
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+	D3D11_BUFFER_DESC ibd = {};
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.CPUAccessFlags = 0u;
+	ibd.MiscFlags = 0u;
+	ibd.ByteWidth = sizeof(indices);
+	ibd.StructureByteStride = sizeof(unsigned short);
+	D3D11_SUBRESOURCE_DATA isd = {};
+	isd.pSysMem = indices;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
+
+	//Bind index buffer
+	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
+
 	//Create pixel shader
 	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
 	wrl::ComPtr<ID3DBlob> pBlob;
@@ -141,10 +186,6 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 	//Bind pixel shader
 	pContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
 
-	//Bind vertex buffer to pipeline
-	const UINT stride = sizeof(Vertex);
-	const UINT offset = 0u;
-	pContext->IASetVertexBuffers( 0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset );
 
 	//Create vertex shader
 	wrl::ComPtr<ID3D11VertexShader> pVertexShader;
@@ -162,6 +203,7 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 	const D3D11_INPUT_ELEMENT_DESC ied[] =
 	{
 		{"Position", 0, DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"Color", 0, DXGI_FORMAT_R8G8B8A8_UNORM,0,8u,D3D11_INPUT_PER_VERTEX_DATA, 0},
 	};
 	GFX_THROW_INFO(pDevice->CreateInputLayout(
 		ied, (UINT)std::size(ied),
@@ -189,7 +231,7 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 	vp.TopLeftY = 0;
 	pContext->RSSetViewports(1u, &vp);
 
-	GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u) );
+	GFX_THROW_INFO_ONLY(pContext->DrawIndexed((UINT)std::size(indices),0u, 0u) );
 }
 //Graphics Exception stuff
 Graphics::HrException::HrException(int line, const char* file, HRESULT hr, std::vector<std::string>infoMsgs ) noexcept
